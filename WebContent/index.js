339a2dedc63e520c77481edf594e1d89609b8966
Reinalyn Conde <rsconde@up.edu.ph>
2020-02-26 22:05:28 +0800

    Give each player a designated timer - Display the high scores given by the backend - Display the result of the game

--- WebContent/index.js
+++ WebContent/index.js
@@ -2,23 +2,36 @@ const BASE_URL = "http://localhost:8080/tictactoe";
 var player1Name = "Player1";
 var player2Name = "Player2";
 var counter = 0;
+var player2Seconds = 0;
+var player2IntervalId = 0;
 var board = [
-	["-", "-", "-"],
-	["-", "-", "-"],
-	["-", "-", "-"]
-]
-var mins = 0;
-var seconds = 0;
-var intervalId = 0;
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+];
+var defaultBoard = [
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+];
+var timer = {
+	"player1" : {
+		"mins" : 0,
+		"seconds" : 0,
+		"isPaused" : false
+	},
+	"player2" : {
+		"mins" : 0,
+		"seconds" : 0,
+		"isPaused" : true
+	}
+};
 
 
 function registerPlayers() {
 	player1Name = $("#player1NameInput").val().length > 0 ? $("#player1NameInput").val() : "Player 1";
 	player2Name = $("#player2NameInput").val().length > 0 ? $("#player2NameInput").val() : "Player 2";
-	
 
-	
-	//TODO:
 	registerToBackend();
 	
 }
@@ -41,7 +54,7 @@ function registerToBackend(){
 	};
 	
 	$.ajax({
-		url: BASE_URL + "/registerPlayers",
+		url: BASE_URL + "/players",
 		type: "POST",
 		crossDomain: true,
 		data: JSON.stringify(players),
@@ -70,21 +83,17 @@ function updateBoard() {
 	let col = (cellNumber % 3) != 0 ? cellNumber % 3 : 3;
 	let pieceToPlace = counter % 2 != 0 ? "X" : "O";
 	
-	if(board[row-1][col-1] === "-"){
+	if(parseInt(board[row-1][col-1]) >= 1 && parseInt(board[row-1][col-1]) <= 9){
 		board[row-1][col-1] = pieceToPlace;
 		$("#gameGrid tbody:nth-child(1) tr:nth-child(" + row + ") td:nth-child(" + col + ")").text(pieceToPlace);
 		$("#cellNumberInput").val("");
 		validateCellNumberInput();
+		sendBoard(row-1, col-1, pieceToPlace);
 		changeTurnCounter();
 	}
 	else{
 		alert("Invalid move");
-	}
-	
-	//TODO:
-	//sendBoard to java for checking
-	//display if there is a winner
-	//stop interval if there is a winner
+	}	
 }
 
 function isInt(n) {
@@ -92,20 +101,40 @@ function isInt(n) {
 }
 
 function changeTurnCounter() {
-	let playerToPlay = counter % 2 == 0 ? player1Name : player2Name;
+	let playerToPlay = "";
+	if(counter % 2 == 0){
+		playerToPlay = player1Name;
+		timer["player1"].isPaused = false;
+		timer["player2"].isPaused = true;
+	}
+	else{
+		playerToPlay = player2Name;
+		timer["player1"].isPaused = true;
+		timer["player2"].isPaused = false;
+	}
+
 	$("#turnCounter").text(playerToPlay + "'s turn");
 	counter += 1;
 }
 
 function startTimer(){
-	intervalId = setInterval(function(){
-		seconds += 1;
-		if(seconds > 60){
-			seconds = 0;
-			mins += 1;
+	if(player1IntervalId === 0 && player2IntervalId === 0){
+		player1IntervalId = startInterval("player1");
+		player2IntervalId = startInterval("player2");
+	}
+}
+
+function startInterval(player){
+	return setInterval(function(){
+		if(!timer[player].isPaused){
+			timer[player].seconds += 1;
+			if(timer[player].seconds > 60){
+				timer[player].seconds = 0;
+				timer[player].mins += 1;
+			}
+
+			$("#" + player + "-time").text("Time: " + get2DigitTime(timer[player].mins) + ":" + get2DigitTime(timer[player].seconds));
 		}
-		
-		$("#time").text("Time: " + mins + ":" + seconds);
 	}, 1000);
 }
 
@@ -121,8 +150,115 @@ function changeDisplay(event){
 }
 
 function displayHighScore(){
-	//TODO:
-	//display highscore from DB
+	$.ajax({
+		url : BASE_URL + "/highscores",
+		type: "GET",
+		crossDomain: true,
+		success : function(result){
+			appendHighScores(JSON.parse(result));
+		},
+		error : function(error){
+			console.log(error);
+		}
+	})
+}
+
+function sendBoard(row, col, latestMove){
+	let boardState = {
+		"row" : row,
+		"col" : col,
+		"latestMove" : latestMove,
+		"board" : board,
+		"player1Time" : "00:" + get2DigitTime(timer["player1"].mins) + ":" + get2DigitTime(timer["player1"].seconds),
+		"player2Time" : "00:" + get2DigitTime(timer["player2"].mins) + ":" + get2DigitTime(timer["player2"].seconds)
+	};
+
+	$.ajax({
+		url : BASE_URL + "/move",
+		type : "POST",
+		crossDomain : true,
+		data: JSON.stringify(boardState),
+		contentType: "application/json",
+		success: function(result){
+			displayResults(result);
+		},
+		error: function(error){
+			console.log(error);
+		}
+	})
+}
+
+function displayResults(winner){
+	if(winner === "-")
+		return;
+	else if(winner === "draw")
+		alert("It's a draw!");
+	else if(winner === "X")
+		alert(player1Name + " has won!");
+	else
+		alert(player2Name + " has won!");
+
+	resetVariables();
+	resetUI();
+}
+
+function resetUI(){
+	$("#gameHolder-div").css("visibility", "hidden");
+	$("#player1NameInput").attr("disabled", false);
+	$("#player2NameInput").attr("disabled", false);
+	$("#registerBtn").attr("disabled", false);
+	$(".tab").attr("disabled", false);
+	$("#player1-time").text("Time: 00:00");
+	$("#player2-time").text("Time: 00:00");
+
+	for(let i=0; i<board.length; i++){
+		for(let j=0; j<board[i].length; j++){
+			$("#gameGrid tbody:nth-child(1) tr:nth-child(" + (i+1) + ") td:nth-child(" + (j+1) + ")").text(board[i][j]);
+		}
+	}
+
+}
+
+function resetVariables(){
+	board = defaultBoard;
+
+	timer = {
+		"player1" : {
+			"mins" : 0,
+			"seconds" : 0,
+			"isPaused" : false
+		},
+		"player2" : {
+			"mins" : 0,
+			"seconds" : 0,
+			"isPaused" : true
+		}
+	}
+
+
+	clearInterval(player1IntervalId);
+	clearInterval(player2IntervalId);
+
+	player1IntervalId = 0;
+	player2IntervalId = 0;
+}
+
+function get2DigitTime(timeString){
+	return timeString > 9 ? timeString : "0" + timeString;
+}
+
+function appendHighScores(highscores){
+	let names = Object.keys(highscores);
+	let tbody = $("#highscore-table tbody");
+	console.log(tbody);
+
+	for(let i=0; i<names.length; i++){
+		let tr = $("<tr>", {});
+		tr.append($("<td>", {}).text(names[i]));
+		tr.append($("<td>", {}).text(highscores[names[i]]));
+
+		tbody.append(tr);
+	}
 }
 
 $(document).ready(function() {
